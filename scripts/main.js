d40cc7091efdfa56146a5ddb5ee5dfb44fa66939
Alia Adel <alia.adel.83@gmail.com>
2018-06-25 16:12:14 +0200

    fix: trying fix the slider arrows status

--- scripts/main.js
+++ scripts/main.js
@@ -2,22 +2,48 @@
  * @description Change Home page slider's arrows active status
  */
 function updateSliderArrowsStatus(
-    slidesDiv,
+    cardsContainer,
     containerWidth,
     cardCount,
     cardWidth
 ) {
-    if ($(slidesDiv).scrollLeft() + containerWidth < cardCount * cardWidth) {
+
+    console.log(`
+    ********************************
+    Container: ${cardsContainer}
+    Conatiner Width: ${containerWidth}
+    Card Count: ${cardCount}
+    Card Width: ${cardWidth}
+    `);
+    console.log(`
+    Is there items on the right?
+    Is ${$(cardsContainer).scrollLeft() + containerWidth} < ${(cardCount * cardWidth) + 15}
+    `);
+    if ($(cardsContainer).scrollLeft() + containerWidth < (cardCount * cardWidth) + 15) {
+        console.log('Yes, add right active class');
         $('#slide-right-container').addClass("active");
     } else {
         $('#slide-right-container').removeClass("active");
     }
 
-    if ($(slidesDiv).scrollLeft() + containerWidth > containerWidth) {
+    console.log(`
+    Is there items on the left?
+    Is ${$(cardsContainer).scrollLeft() + containerWidth} > ${containerWidth}
+    `);
+
+    if ($(cardsContainer).scrollLeft() > 0) {
+        console.log('Yes, add left active class');
         $('#slide-left-container').addClass("active");
     } else {
         $('#slide-left-container').removeClass("active");
     }
+}
+
+/**
+ * @description fill products' cards
+ */
+function loadCards(tabID, cards){
+
 }
 
 $(function () {
@@ -34,34 +60,36 @@ $(function () {
     // Scroll products' slider left/right        
     let div = $('#productsTabContent');
     let cardCount = $(div).find('.tab-pane').children('.app-card-container').length;
-    let speed = 1500;
+    let speed = 1000;
     let containerWidth = $('#products-slider').width();
     let cardWidth = 250;
+    
     updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
 
     //Remove scrollbars    
-    $('#slide-right-container').click(function (e) {
-        if (($(div).scrollLeft() + containerWidth) < (cardCount * cardWidth)) {
+    //TODO theres still an issue in activating the arrows
+    $('#slide-right-container').click(function (e) {        
+        if (($(div).scrollLeft() + containerWidth) < (cardCount * cardWidth) + 15) {
             $(div).animate({
-                scrollLeft: $(div).scrollLeft() + containerWidth
-            }, speed,
-                updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth));
-        }else {
-            updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
-        }        
+                scrollLeft: $(div).scrollLeft() + cardWidth
+            }, {
+                duration: speed,
+                complete: updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth)
+            });
+        }  
+        updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
     });
-    $('#slide-left-container').click(function (e) {
+    $('#slide-left-container').click(function (e) {        
         if (($(div).scrollLeft() + containerWidth) > containerWidth) {
             $(div).animate({
-                scrollLeft: '-=' + containerWidth
-            }, speed,
-                updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth));
-        } else {
-            updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
-        }        
+                scrollLeft: '-=' + cardWidth
+            },  {
+                duration: speed,
+                complete: updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth)
+            });            
+        }     
+        updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
     });
-
-
 
 
     // If resize action ocurred then update the container width value
