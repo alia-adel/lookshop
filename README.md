8286f642f2dfada957fb02b9027394ca58b5cdd6
Alia Adel <alia.adel.83@gmail.com>
2018-06-25 14:22:15 +0200

    feat: Add slider arrows to products cards

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Simple non working e-commerce using HTML5, CSS3, JavaScript, Bootstrap 4 &amp; jQuery
 - Design credits to **Pavel Capcan** PSD on Behance[https://www.behance.net/gallery/10058323/LookShop-Free-Responsive-PSD-Template]
 - Working copy [https://alia-adel.github.io/lookshop/]
+
+** TODO
+- Update symantics to use article for cards
+- Home page slider tabs should be transfered to dropdown list
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,25 @@
+/**
+ * @description Change Home page slider's arrows active status
+ */
+function updateSliderArrowsStatus(
+    slidesDiv,
+    containerWidth,
+    cardCount,
+    cardWidth
+) {
+    if ($(slidesDiv).scrollLeft() + containerWidth < cardCount * cardWidth) {
+        $('#slide-right-container').addClass("active");
+    } else {
+        $('#slide-right-container').removeClass("active");
+    }
+
+    if ($(slidesDiv).scrollLeft() + containerWidth > containerWidth) {
+        $('#slide-left-container').addClass("active");
+    } else {
+        $('#slide-left-container').removeClass("active");
+    }
+}
+
 $(function () {
     // Toggle search box
     $('.app-search').click(() => {
@@ -7,5 +29,49 @@ $(function () {
     //Toggle my cart window
     $('.mycart-header').click(() => {
         $('#mycart').toggleClass('active');
+    });
+
+    // Scroll products' slider left/right        
+    let div = $('#productsTabContent');
+    let cardCount = $(div).find('.tab-pane').children('.app-card-container').length;
+    let speed = 1500;
+    let containerWidth = $('#products-slider').width();
+    let cardWidth = 250;
+    updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
+
+    //Remove scrollbars    
+    $('#slide-right-container').click(function (e) {
+        if (($(div).scrollLeft() + containerWidth) < (cardCount * cardWidth)) {
+            $(div).animate({
+                scrollLeft: $(div).scrollLeft() + containerWidth
+            }, speed,
+                updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth));
+        }else {
+            updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
+        }        
+    });
+    $('#slide-left-container').click(function (e) {
+        if (($(div).scrollLeft() + containerWidth) > containerWidth) {
+            $(div).animate({
+                scrollLeft: '-=' + containerWidth
+            }, speed,
+                updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth));
+        } else {
+            updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
+        }        
+    });
+
+
+
+
+    // If resize action ocurred then update the container width value
+    $(window).resize(function () {
+        try {
+            containerWidth = $('#products-slider').width();
+            updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
+        } catch (error) {
+            console.log(`Error occured while trying to get updated slider container width: 
+            ${error}`);
+        }
     });
 });
