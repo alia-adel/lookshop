cf1dfd324c5c1a7f64d6031d7f85f2e10671f1dc
alia-adel <alia.adel.83@gmail.com>
2019-07-07 16:28:50 +0400

    feat: products tab

--- scripts/main.js
+++ scripts/main.js
@@ -103,3 +103,32 @@ $(function () {
         }
     });
 });
+
+// New
+$('#hot-tab > .wrapper').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 4,
+    arrows: false,
+    lazyLoad: 'ondemand',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
